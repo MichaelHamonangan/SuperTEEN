594d4b58ab2311c828789a0f102646fa3afacd7d
MichaelHamonangan <hamonanganmichael@mail.ugm.ac.id>
2021-04-30 14:22:47 +0700

    commit

---
+++ ModulDatabase/KesehatanData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Data;
+using System.Data.OleDb;
+using System.Collections.Generic;
+
+namespace ModulDatabase
+{
+    //class akan sejumlah 3 untuk 3 modul yaitu kesehatan, belajar dan motivasi
+    public class KesehatanData
+    {
+        private static OleDbConnection dbConnection;
+        private static OleDbCommand dbCommand;
+        private static OleDbDataReader dataReader;
+
+        private static string conn;
+        public static void SetConnectionString(string connection)
+        {
+            conn = connection;
+        }
+
+        //data + polymorphism disini
+        
+        public KesehatanData()
+        {
+            //constructor
+        }
+
+
+        public int Create()
+        {
+            return 0;
+        }
+
+        public static KesehatanData[] Read()
+        {
+            //membaca data
+            return null;
+        }
+        public int Update(int id)
+        {
+            return 0;
+        }
+        public int Delete(int id)
+        {
+            return 0;
+        }
+    }
+}
